485c053db155d7f455c6a3a4c4e37e5acb0c4f1c
manikandareas <vitoandareas15@gmail.com>
2023-12-19 02:52:51 +0800

    feat: adding nest js

---
+++ apps/web/next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  transpilePackages: ["@repo/ui"],
+  async rewrites ( ) {
+    return [
+      // Rewrites all API Requests to your Nest server
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:8000/api/:path*"
+      }
+    ]
+  }
+};
