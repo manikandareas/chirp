97eb57595cf05fb69ccb75a55937e749c60c555c
Farhan Dwi Hartanto <farhandwihartanto@gmail.com>
2023-12-29 13:53:42 +0800

    fix(dto): fixing the user password must be at least 8 characters long and must not start and end with a space

--- packages/dto/src/user/create-user.dto.ts
+++ packages/dto/src/user/create-user.dto.ts
@@ -4,8 +4,23 @@ import {
   IsNotEmpty,
   IsOptional,
   IsString,
+  Matches,
+  MinLength,
 } from "class-validator";
 
+/**
+ * This class definition is for a CreateUserDto object. Here's a summary of what each class method does:
+ *
+ * firstName: Validates that the first name is not empty and is a string.
+ * lastName: Validates that the last name is not empty and is a string.
+ * username: Validates that the username is not empty and is a string.
+ * dob: Validates that the date of birth is not empty and is a string.
+ * email: Validates that the email is not empty and is a valid email format.
+ * password: Validates that the password is not empty, is at least 8 characters long, and does not start or end with whitespace.
+ * image: Validates that the image is an optional string.
+ * gender: Validates that the gender is not empty and is either "male" or "female".
+ * address: Validates that the address is an optional string.
+ */
 export class CreateUserDto {
   @IsNotEmpty()
   @IsString()
@@ -29,6 +44,10 @@ export class CreateUserDto {
 
   @IsNotEmpty()
   @IsString()
+  @Matches(/^[^\s].*[^\s]$/, {
+    message: "Password cannot start and end with whitespace",
+  })
+  @MinLength(8)
   password: string;
 
   @IsOptional()
